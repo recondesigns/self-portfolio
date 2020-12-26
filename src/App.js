c2601e4b6355122a0bfeb22d1ba629d2cb749873
Michael Stedman <michaelstedman81@gmail.com>
2020-12-25 23:11:27 -0800

    Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const AppContainer = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
   border: 2px solid lightcoral;
 `
 
