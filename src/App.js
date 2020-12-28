1a2373b8c984e3f24bce24b893d77d0241a56269
Michael Stedman <michaelstedman81@gmail.com>
2020-12-28 10:21:40 -0800

    Updated App.js
    Added react-router-dom and is functioning and changing routes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Switch, Route } from 'react-router-dom'
 import { Header } from './components/Header'
 import Footer from './components/Footer'
 import Landing from './pages/Landing'
 
-// import Developer from './pages/Developer'
-// import UxDesigner from './pages/UxDesigner'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
+import Developer from './pages/Developer'
+import UxDesigner from './pages/UxDesigner'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const AppContainer = styled.div`
   box-sizing: border-box;
@@ -20,7 +21,27 @@ export default function App() {
   return (
     <AppContainer>
       <Header />
-      <Landing />
+      <Switch>
+        <Route exact path='/home'>
+          <Landing />
+        </Route>
+          
+        <Route path='/developer'>
+          <Developer />
+        </Route>
+
+        <Route path='/uxdesigner'>
+          <UxDesigner />
+        </Route>
+
+        <Route path='/about'>
+          <About />
+        </Route>
+
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+      </Switch>
       <Footer />
     </AppContainer>
   )
